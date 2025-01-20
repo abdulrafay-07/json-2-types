935b0d4eed232517e8b4bdb6a640ee22b6f437cf
Abdul Rafay <abdulrafayaslam.007@gmail.com>
2025-01-20 10:29:20 +0500

    feat: add compatibility with objects, nested objects

--- src/main.ts
+++ src/main.ts
@@ -6,45 +6,65 @@ const copyBtn = document.getElementById("copy-btn") as HTMLButtonElement;
 const jsonOutput = document.getElementById("json-output") as HTMLTextAreaElement;
 const textareaInput = document.getElementById("json-input") as HTMLTextAreaElement;
 
-function getType(value: any): string {
+// TODO list: check for array with multiple types, check for optional values, check for 2d array types
+
+function getType(value: any, indentLevel = 1): string {
+  const indent = "\t".repeat(indentLevel);
+
   if (typeof value === "string") {
-    return "string;";
+    return "string";
   } else if (typeof value === "number") {
-    return "number;";
+    return "number";
   } else if (typeof value === "boolean") {
-    return "boolean;";
+    return "boolean";
   } else if (value === null) {
-    return "string | null;";
+    return "string | null";
   } else if (Array.isArray(value)) {
     const firstElement = value[0];
+    if (!firstElement) return "any[]";
+    if (Array.isArray(firstElement)) return "any[][]";
 
     if (typeof firstElement === "string") {
-      return "string[];";
+      return "string[]";
     } else if (typeof firstElement === "number") {
-      return "number[];";
+      return "number[]";
     } else if (typeof firstElement === "boolean") {
-      return "boolean[];";
+      return "boolean[]";
+    } else if (typeof firstElement === "object") {
+      const nestedType = getType(firstElement, indentLevel);
+      return `${nestedType}[]`;
     };
+  } else if (typeof value === "object") {
+    if (Object.keys(value).length === 0) return "{};";
+
+    let objString = "{\n";
+    for (let key in value) {
+      const v = value[key];
+      const type = getType(v, indentLevel + 1);
+      objString += `${indent}${key}: ${type};\n`;
+    };
+    return objString + "\t".repeat(indentLevel - 1) + "}";
   };
 
   return "unknown;";
 };
 
 inputBtn?.addEventListener("click", () => {
-  const input = textareaInput.value;
-  if (input === "" || !input.startsWith("{") || !input.endsWith("}")) {
-    textareaInput.value = "";
-    alert("Invalid input");
-    return;
+  let parsedInput;
+
+  try {
+    parsedInput = JSON.parse(textareaInput.value);
+  } catch (error) {
+    alert("Invalid JSON input. Please check your syntax. (error in console)");
+    console.log(error);
+    return
   };
 
-  const parsedInput = JSON.parse(input);
-  
   const hashMap = new Map();
 
   for (let key in parsedInput) {
     const value = parsedInput[key];
-    const type = getType(value);
+    const type = getType(value, 2);
     hashMap.set(key, type);
   };
 
@@ -52,11 +72,10 @@ inputBtn?.addEventListener("click", () => {
 
   let outputString = 'interface JsonType {';
   for (let k in outputObj) {
-    outputString += "\n\t" + k + ": " + outputObj[k];
+    outputString += "\n\t" + k + ": " + outputObj[k] + ";";
   };
   outputString += "\n};";
 
-  console.log(outputString);
   jsonOutput.value = outputString;
 });
 
